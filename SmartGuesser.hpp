3b230281ae9ad9bcec2036e8d253df5bc1024dca
tomerr1212 <44744800+tomerr1212@users.noreply.github.com>
2019-06-03 14:39:54 +0300

    Update SmartGuesser.hpp

--- SmartGuesser.hpp
+++ SmartGuesser.hpp
@@ -6,9 +6,5 @@ class SmartGuesser: public bullpgia::Guesser {
     public:
         SmartGuesser();
         virtual string guess() override;
-        /* This method recives the string that calculateBullAndPgia returns and learn about the next guess. */
-        virtual void learn(string reply)override;
-        
-
-
+        virtual void learn(string calculate)override;
 };
